1382ffcb78c8689d2edf8b6bd3c93a61bb9fc4dd
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-08-24 14:57:24 +0200

    💚(ci) upgrade Django version to 4.2.4 in the sandbox app
    Fixed CI jobs, sandbox Django settings wasn't set
    for Django version above v4.2. Certain deprecated Django
    settings triggered dependency issues.
    
    Pinning Django dependency version is now exclusively for
    the sandbox app. The library dependencies require no
    specific Django version.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -226,9 +226,11 @@ class Production(Base):
     # For static files in production, we want to use a backend that includes a hash in
     # the filename, that is calculated from the file content, so that browsers always
     # get the updated version of each file.
-    STATICFILES_STORAGE = (
-        "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-    )
+    STORAGE = {
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        }
+    }
 
 
 class Feature(Production):
